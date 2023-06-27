32ee4506dd4c4a19183f7d55585072a6105a8ef3
ARUN_KUMAR_PILLI <103062517+ArunKumarPilli52@users.noreply.github.com>
2023-06-27 13:35:01 +0530

    Add files via upload

---
+++ data types 1 .cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	x=5;
+	cout<<"the value of x is :"<<x;
+	
+	
+	return 0;
+} 
+
+/*  or 
+#include<iostream>
+
+int main()
+{
+	int x;
+	x=5;
+	std::cout<<"the value of x is :"<<x;
+	
+	
+	return 0;
+}
+*/
