971e5ba84a8953d001c061993a6ecf8d0c9d9008
ARUN_KUMAR_PILLI <103062517+ArunKumarPilli52@users.noreply.github.com>
2023-07-06 00:19:51 +0530

    Add files via upload

---
+++ patterns/pattern 6.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cout<<"enter n value:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"* ";
+    }
+    cout<<endl;
+    for(int k=1;k<=n-1;k++)
+    {
+        cout<<"* ";
+        for(int j=1;j<=n-1;j++)
+        {
+            cout<<" ";
+        }
+        for(int l=1;l<=1;l++)
+        {
+            cout<<"  *";
+        }
+        cout<<endl;
+        
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"* ";
+    }
+    return 0;
+}
