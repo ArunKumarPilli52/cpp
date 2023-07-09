23539d26a69f798a5ead88b1395480bf7319f448
ARUN_KUMAR_PILLI <103062517+ArunKumarPilli52@users.noreply.github.com>
2023-07-09 10:47:56 +0530

    Add files via upload

---
+++ patterns/pattern 9.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   cout<<"enter a number:";
+   cin>>x;
+   for( int i=1;i<=1;i++)
+   {
+       cout<<" "<<i<<endl;
+       cout<<" "<<i+1<<" "<<i+2<<endl;
+       cout<<" "<<i+3<<" "<<i+4<<" "<<i+5<<endl;
+       cout<<" "<<i+6<<" "<<i+7<<" "<<i+8<<" "<<i+9<<endl;
+   }
+   return 0;
+}
